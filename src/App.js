68030623c6e5362eaccd2b727143a6bcdbc596e2
Shibatuch <Schibatuch@gmail.com>
2021-05-29 21:21:17 +0300

    upgrade

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-        <li>React</li>
-      </ul>
+      <Header />
+      <Technologies />
     </div>
   );
+}
+
+const Technologies = () => {
+  return (
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JS</li>
+      <li>React</li>
+    </ul>
+  )
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">News Feed</a>
+      <a href="#">Messages</a>
+    </div>
+  )
 }
 
 export default App;
