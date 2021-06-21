54a59707ad21438edbdef80b3b091b9d1cba6ddd
Shibatuch <Schibatuch@gmail.com>
2021-06-21 23:32:08 +0300

    add router and new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import "./App.css";
-import Profile from "./components/Profile/Profile";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
+import './App.css';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs'
+import News from './components/News/News'
+import Music from './components/Music/Music'
+import Settings from './components/Settings/Settings'
+import {Route, BrowserRouter} from 'react-router-dom';
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/setting' component={Settings}/>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
