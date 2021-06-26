7a35fa79646667ba186aa97df6839e09947c280f
Shibatuch <Schibatuch@gmail.com>
2021-06-26 13:53:23 +0300

    add fake redux

--- src/index.js
+++ src/index.js
@@ -2,64 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-let posts = [
-  {
-    id: 1,
-    message: 'Hi, how are you?',
-    likesCount: 22
-  },
-  {
-    id: 2,
-    message: 'It\'s my first post',
-    likesCount: 34
-  }
-];
-
-let dialogs = [
-  {
-    id: 1,
-    name: 'Timofey'
-  },
-  {
-    id: 2,
-    name: 'Kostya'
-  },
-  {
-    id: 3,
-    name: 'Kolyas'
-  },
-  {
-    id: 4,
-    name: 'Dim'
-  },
-  {
-    id: 5,
-    name: 'Dasha'
-  }
-];
-
-let messages = [
-  {
-    id: 1,
-    message: 'Hi'
-  },
-  {
-    id: 2,
-    message: 'Hello'
-  },
-  {
-    id: 3,
-    message: 'Have a nice day!'
-  }
-];
+import state from './redux/state'
 
 ReactDOM.render(
   <React.StrictMode>
     <App 
-      posts={posts}
-      dialogs={dialogs}
-      messages={messages}
+      state={state}
     />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/redux/state.js
@@ -0,0 +1,56 @@
+let state = {
+  profilePage: {
+    posts: [
+      {
+        id: 1,
+        message: 'Hi, how are you?',
+        likesCount: 22
+      },
+      {
+        id: 2,
+        message: 'It\'s my first post',
+        likesCount: 34
+      }
+    ]
+  },
+  dialogsPage: {
+    dialogs: [
+      {
+        id: 1,
+        name: 'Timofey'
+      },
+      {
+        id: 2,
+        name: 'Kostya'
+      },
+      {
+        id: 3,
+        name: 'Kolyas'
+      },
+      {
+        id: 4,
+        name: 'Dim'
+      },
+      {
+        id: 5,
+        name: 'Dasha'
+      }
+    ],
+    messages: [
+      {
+        id: 1,
+        message: 'Hi'
+      },
+      {
+        id: 2,
+        message: 'Hello'
+      },
+      {
+        id: 3,
+        message: 'Have a nice day!'
+      }
+    ]
+  }
+};
+
+export default state;
