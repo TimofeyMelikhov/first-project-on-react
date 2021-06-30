705f774c4ea054e10f2d610390afc2afdd6a71cc
Shibatuch <Schibatuch@gmail.com>
2021-06-30 15:42:12 +0300

    fix style

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import News from './components/News/News'
 import Music from './components/Music/Music'
 import Settings from './components/Settings/Settings'
 import {Route} from 'react-router-dom';
+
 function App(props) {
   return (
-  
     <div className="app-wrapper">
       <Header />
       <Navbar />
