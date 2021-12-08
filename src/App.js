63b7071f37e01bb38364cdf4292c2b264667b04d
Shibatuch <Schibatuch@gmail.com>
2021-12-08 23:43:51 +0300

    fix bag and start flux

--- src/App.js
+++ src/App.js
@@ -9,34 +9,20 @@ import Settings from './components/Settings/Settings'
 import {Route} from 'react-router-dom';
 
 function App(props) {
-  return (
-<<<<<<< HEAD
-    
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage}/> }/>
-          <Route path='/profile' render={ () => <Profile profilePage={props.state.profilePage} 
-            addPost={props.addPost}
-            updateNewPostText={props.updateNewPostText}
-            />}
-            />
-          <Route path='/news' render={ () => <News />}/>
-          <Route path='/music' render={ () => <Music />}/>
-          <Route path='/setting' render={ () => <Settings />}/>
-        </div>
-=======
+  return (    
     <div className="app-wrapper">
       <Header />
       <Navbar />
       <div className='app-wrapper-content'>
         <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage}/> }/>
-        <Route path='/profile' render={ () => <Profile state={props.state.profilePage} addPost={props.addPost}/>}/>
+        <Route path='/profile' render={ () => <Profile profilePage={props.state.profilePage} 
+          addPost={props.addPost}
+          updateNewPostText={props.updateNewPostText}
+          />}
+          />
         <Route path='/news' render={ () => <News />}/>
         <Route path='/music' render={ () => <Music />}/>
         <Route path='/setting' render={ () => <Settings />}/>
->>>>>>> 705f774c4ea054e10f2d610390afc2afdd6a71cc
       </div>
     </div>
   );
