1a25d397e7690c19b292afb993f872d90314422c
Shibatuch <Schibatuch@gmail.com>
2021-12-09 14:13:25 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ function App(props) {
       <Navbar />
       <div className='app-wrapper-content'>
         <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage}
-          newTextMessage={props.state.dialogsPage.newTextMessage}
-          sendMessage={props.sendMessage}
-          updateNewMessageText={props.updateNewMessageText}
-        />}
+            newTextMessage={props.state.dialogsPage.newTextMessage}
+            sendMessage={props.sendMessage}
+            updateNewMessageText={props.updateNewMessageText}
+          />}
         />
         <Route path='/profile' render={ () => <Profile profilePage={props.state.profilePage} 
           addPost={props.addPost}
           updateNewPostText={props.updateNewPostText}
           />}
-          />
+        />
         <Route path='/news' render={ () => <News />}/>
         <Route path='/music' render={ () => <Music />}/>
         <Route path='/setting' render={ () => <Settings />}/>
